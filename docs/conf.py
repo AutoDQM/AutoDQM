9c09302544c6140fdfb79e8e07e67019cf4264c4
Robert White <rwhite1@lx01.hep.ph.ic.ac.uk>
2020-07-06 10:03:46 +0100

    Adding files to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+import autodqm
+import autoref
 
 # -- Project information -----------------------------------------------------
 
@@ -65,3 +66,5 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+htmlhelp_basename = 'auto-dqm-doc'
