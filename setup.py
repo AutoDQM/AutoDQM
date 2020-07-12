3249e47f60935bf8181d661d8818579fa4ce22fe
Robert White <rwhite1@lx01.hep.ph.ic.ac.uk>
2020-07-12 16:15:25 +0100

    Adding setup file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_version():
         exec(version_file.read(), _globals)
     return _globals["__version__"]
 
-requirements = ['certifi=2018.4.16', 'chardet==3.0.4', 'idna==2.7', 'lxml==4.2.3',
+requirements = ['-i https://pypi.org/simple','certifi=2018.4.16', 'chardet==3.0.4', 'idna==2.7', 'lxml==4.2.3',
                 'requests-futures==0.9.7', 'requests==2.19.1', 'tqdm==4.23.4',
                 'urllib3==1.2.3', 'python_version>=2.6']
 
