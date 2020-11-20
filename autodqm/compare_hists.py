8b96a820042bc7f52abd5dd5578047bd0fe080d6
Robert White <rwhite1@lx02.hep.ph.ic.ac.uk>
2020-11-20 13:28:17 +0000

    Testing to read module

--- autodqm/compare_hists.py
+++ autodqm/compare_hists.py
@@ -208,3 +208,14 @@ def identifier(hp, comparator_name):
 
 class error(Exception):
     pass
+
+def get_subsystem(cfg_dir, subsystem):
+    """Return the dict-based configuration of subsystem from cfg_dir."""
+    fname = subsystem + '.json'
+    path = os.path.join(cfg_dir, fname)
+    if not os.path.exists(path):
+        raise error("Subsystem config '{0}' not found.".format(subsystem))
+
+    with open(os.path.join(cfg_dir, fname)) as f:
+        return json.load(f)
+
