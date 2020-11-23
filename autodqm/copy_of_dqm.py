884adfd820dd3638498e50de090a0e12e275c692
Robert White <rwhite1@lx02.hep.ph.ic.ac.uk>
2020-11-23 09:45:20 +0000

    Commenting headers out

--- autodqm/copy_of_dqm.py
+++ autodqm/copy_of_dqm.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import base64
-import errno
-import json
-import lxml.html
-import os
-import requests
-from collections import namedtuple
-from requests_futures.sessions import FuturesSession
+#import base64
+#import errno
+#import json
+#import lxml.html
+#import os
+#import requests
+#from collections import namedtuple
+#from requests_futures.sessions import FuturesSession
 
-TIMEOUT = 5
+#TIMEOUT = 5
 
-BASE_URL = 'https://cmsweb.cern.ch'
-DQM_URL = 'https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/'
-CA_URL = 'https://cafiles.cern.ch/cafiles/certificates/CERN%20Root%20Certification%20Authority%202.crt'
+#BASE_URL = 'https://cmsweb.cern.ch'
+#DQM_URL = 'https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/'
+#CA_URL = 'https://cafiles.cern.ch/cafiles/certificates/CERN%20Root%20Certification%20Authority%202.crt'
 
 # The following are appended to the db dir
-CACHE_DIR = 'cache/'
-CA_PATH = 'CERN_Root_CA.crt'
+#CACHE_DIR = 'cache/'
+#CA_PATH = 'CERN_Root_CA.crt'
 
-StreamProg = namedtuple('StreamProg', ('cur', 'total', 'path'))
-DQMRow = namedtuple('DQMRow', ('name', 'full_name', 'url', 'size', 'date'))
+#StreamProg = namedtuple('StreamProg', ('cur', 'total', 'path'))
+#DQMRow = namedtuple('DQMRow', ('name', 'full_name', 'url', 'size', 'date'))
 
 
 class DQMSession(FuturesSession):
     """
     Encapsulates an interface to DQM Offline.
+    Commenting stuff out as well as removing functions should fix this
+
     """
 
     def __init__(self, cert, db, cache=None, workers=16):
