86d199360f6c93713efd469965cb766d4a2365a2
Kaitlin Salyer <ksalyer@nd.edu>
2021-01-21 03:59:02 +0100

    RPC PCA

--- plugins/pca.py
+++ plugins/pca.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-#!/usr/bin/env python2
-=======
 #/!usr/bin/env python3
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
 # -*- coding: utf-8 -*-
 
 import os
 import glob
-<<<<<<< HEAD
-import cPickle as pickle
-import numpy as np
-from sklearn.decomposition import PCA
-import ROOT
-
-=======
 import pickle as pickle
 import numpy as np
 #from sklearn.decomposition import PCA
@@ -23,7 +12,6 @@ sys.path.append('/var/www/cgi-bin/')
 from modules.histCollection import HistCollection
 from modules.histCollection import HistCleaner
 from modules.dqmpca import DQMPCA
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
 from autodqm.plugin_results import PluginResults
 
 def comparators():
@@ -39,21 +27,11 @@ def pca(histpair,
     data_hist = histpair.data_hist.Clone()
     jar_dir = histpair.config["jar_dir"]
 
-<<<<<<< HEAD
-    # Check for unique Pickle file
-    possible_pickles = glob.glob("/var/www/cgi-bin/pickle_jar/{0}/*_{1}.pkl".format(jar_dir, data_name))
-    if len(possible_pickles) != 1:
-        return None
-
-    # Load Pickle file
-    pca_dict = pickle.load(open(possible_pickles[0], "rb"))
-=======
     data_year = int(histpair.data_series[-4:])
     possible_pickles = glob.glob("/var/www/cgi-bin/models/pca/{0}/{1}/*_{2}.pkl".format(jar_dir,data_year, data_name))
     if len(possible_pickles) != 1:
         return None
     pca_pickle = pickle.load(open(possible_pickles[0], "rb"))
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
 
     # Check that the hist is a histogram
     if not data_hist.InheritsFrom('TH1'):
@@ -65,22 +43,6 @@ def pca(histpair,
 
     # Reject empty histograms
     is_good = data_hist.GetEntries() != 0 and data_hist.GetEntries() >= min_entries
-<<<<<<< HEAD
-
-    np_data = get_np_data(data_hist)
-    # Get 'good' (non-zero) bins
-    np_data = np_data[pca_dict["good_bins"]]
-
-    n_components = get_components(exp_var, pca_dict["pca"].explained_variance_ratio_)
-    sse, reco_data = PCATest(np_data, pca_dict["pca"], n_components)
-
-    # Get SSE cut
-    sse_cut = pca_dict["sses_{}comp".format(n_components)]["{}pct".format(sse_percentile)]
-
-    is_outlier = is_good and bool(sse > sse_cut)
-
-    c, artifacts = draw_same(data_hist, reco_data, pca_dict["good_bins"], histpair.data_run, is_outlier)
-=======
     # Reject empty histograms
     is_good = data_hist.GetEntries() != 0 and data_hist.GetEntries() >= min_entries
     np_data = get_np_data(data_hist)
@@ -104,7 +66,6 @@ def pca(histpair,
     is_outlier = is_good and bool(sse > sse_cut)
 
     c, artifacts = draw_same(data_hist, reco_data, pca_pickle.__dict__['_hist_cleaner'].good_bins, histpair.data_run, is_outlier)
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
 
     info = {
         'Data_Entries': data_hist.GetEntries(),
@@ -118,15 +79,12 @@ def pca(histpair,
             info=info,
             artifacts=artifacts)
 
-<<<<<<< HEAD
-=======
 def get_np_data(data_hist):
     """Turn TH1F bin content into a numpy array"""
     np_data = []
     for x in range(1, data_hist.GetNbinsX() +1):
         np_data.append(data_hist.GetBinContent(x))
     return np.array(np_data)
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
 def PCATest(np_data, pca_obj, n_components):
 
     # Transform data in terms of principle component vectors
@@ -141,40 +99,12 @@ def PCATest(np_data, pca_obj, n_components):
 
     return sse, reco_data
 
-<<<<<<< HEAD
-def get_components(exp_var, exp_var_ratios_, n_cap=3):
-    """Get PCA components that explain variance to some percentage (exp_var)"""
-
-    sum_var = 0
-    n_components = 0
-    for var_ratio in exp_var_ratios_:
-        n_components += 1
-        sum_var += var_ratio
-        if sum_var >= exp_var or n_components >= n_cap:
-            return n_components 
-
-    return n_components
-
-def get_np_data(data_hist):
-    """Turn TH1F bin content into a numpy array"""
-    np_data = []
-    for x in range(1, data_hist.GetNbinsX() +1):
-        np_data.append(data_hist.GetBinContent(x))
-
-    return np.array(np_data)
-
-=======
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
 def draw_same(data_hist, reco_data, reco_bins, data_run, is_outlier):
     # Set up canvas
     c = ROOT.TCanvas('c', 'c')
     reco_hist = data_hist.Clone("reco_hist")
     reco_hist.Reset()
-<<<<<<< HEAD
-
-=======
     
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
     # Fill Reco hist
     for i in range(0, len(reco_bins)):
         reco_hist.SetBinContent(int(reco_bins[i])+1, float(reco_data[i]))
@@ -214,11 +144,7 @@ def draw_same(data_hist, reco_data, reco_bins, data_run, is_outlier):
     # Draw legend
     legend = ROOT.TLegend(0.9,0.9,0.75,0.75)
     legend.AddEntry(data_hist, "Data")
-<<<<<<< HEAD
-    legend.AddEntry(reco_hist, "Reco")
-=======
     legend.AddEntry(reco_hist, "PCA Reco")
->>>>>>> 55d456c2750ceb99f4abbaf514fcb43ba26de2ad
     legend.SetBorderSize(0)
     legend.SetFillStyle(0)
     legend.Draw("same")
