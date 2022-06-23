bf0d16ae0aeef05926b80fbaa5eac8135a2a57bb
Chosila Sutantawibul <si_sutantawibul1@baylor.edu>
2022-06-23 18:56:58 +0200

    added both plotting but still broke

--- plugins/beta_binomial.py
+++ plugins/beta_binomial.py
@@ -16,6 +16,9 @@ def beta_binomial(histpair, pull_cap=40, chi2_cut=100, pull_cut=25, min_entries=
 
     """beta_binomial works on both 1D and 2D"""
 
+    print(histpair.data_name)
+
+
     data_hist = histpair.data_hist
     ref_hist = histpair.ref_hist
 
@@ -30,6 +33,19 @@ def beta_binomial(histpair, pull_cap=40, chi2_cut=100, pull_cut=25, min_entries=
 
     # Reject empty and low stat hist
     is_good = data_hist_Entries > min_entries
+
+
+    # Normalize data_hist (Note if col is selected numpy just transposes normalizes by rows then transposes again)
+    data_hist_norm = numpy.copy(data_hist.values())
+    ref_hist_norm = numpy.copy(ref_hist.values())
+    if norm_type == "row":
+        data_hist_norm = normalize_rows(data_hist_norm, ref_hist_norm)
+    elif norm_type == "col":
+        data_hist_norm = normalize_rows(numpy.transpose(data_hist_norm), numpy.transpose(ref_hist_norm))
+        data_hist_norm = numpy.transpose(data_hist_norm)
+    else:
+        if data_hist_Entries > 0:
+            data_hist_norm = data_hist_norm * ref_hist_Entries / data_hist_Entries
 
     ## only filled bins used for chi2
     nBinsUsed = numpy.count_nonzero(numpy.add(ref_hist_raw.sum(axis=0), data_hist_raw))
@@ -52,60 +68,116 @@ def beta_binomial(histpair, pull_cap=40, chi2_cut=100, pull_cut=25, min_entries=
     ## plotting
     # Setting empty bins to be blank
     pull_hist = numpy.where(pull_hist < -2*pull_cap, None, pull_hist)
-    colors = ['rgb(26, 42, 198)', 'rgb(118, 167, 231)', 'rgb(215, 226, 194)', 'rgb(212, 190, 109)', 'rgb(188, 76, 38)']
 
-    #Getting Plot labels for x-axis and y-axis as well as type (linear or categorical)
-    xLabels = None
-    yLabels = None
-    c = None
-    x_axis_type = 'linear'
-    y_axis_type = 'linear';
-    if data_hist.axes[0].labels():
-       xLabels = [str(x) for x in data_hist.axes[0].labels()]
-       x_axis_type = 'category'
-    else:
-       xLabels = [str(data_hist.axes[0]._members["fXmin"] + x * (data_hist.axes[0]._members["fXmax"]-data_hist.axes[0]._members["fXmin"])/data_hist.axes[0]._members["fNbins"]) for x in range(0,data_hist.axes[0]._members["fNbins"]+1)]
 
-    if data_hist.axes[1].labels():
-       yLabels = [str(x) for x in data_hist.axes[1].labels()]
-       y_axis_type = 'category'
-    else:
-       yLabels = [str(data_hist.axes[1]._members["fXmin"] + x * (data_hist.axes[1]._members["fXmax"]-data_hist.axes[1]._members["fXmin"])/data_hist.axes[1]._members["fNbins"]) for x in range(0,data_hist.axes[1]._members["fNbins"]+1)]
-
-    if("xlabels" in histpair.config.keys()):
-        xLabels=histpair.config["xlabels"]
-        x_axis_type = 'category'
-    if("ylabels" in histpair.config.keys()):
-        yLabels=histpair.config["ylabels"]
-        y_axis_type = 'category'
-
-    pull_hist = numpy.transpose(pull_hist)
-
-    #Getting Plot Titles for histogram, x-axis and y-axis
-    xAxisTitle = data_hist.axes[0]._bases[0]._members["fTitle"]
-    yAxisTitle = data_hist.axes[1]._bases[0]._members["fTitle"]
-    plotTitle = histpair.data_name + " Pull Values  |  data:" + str(histpair.data_run) + " & ref:" + str(histpair.ref_run)
-
-    #Plotly doesn't support #circ, #theta, #phi but does support unicode
-    xAxisTitle = xAxisTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
-    yAxisTitle = yAxisTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
-    plotTitle = plotTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
-
-    #Plot pull-values using 2d heatmap will settings to look similar to old Pyroot version
-    c  = go.Figure(data=go.Heatmap(z=pull_hist, zmin=-pull_cap, zmax=pull_cap, colorscale=colors, x=xLabels, y=yLabels))
-    c['layout'].update(plot_bgcolor='white')
-    c.update_xaxes(showline=True, linewidth=2, linecolor='black', mirror=True, showgrid=False, type=x_axis_type)
-    c.update_yaxes(showline=True, linewidth=2, linecolor='black', mirror=True, showgrid=False, type=y_axis_type)
-    c.update_layout(
-        title=plotTitle , title_x=0.5,
-        xaxis_title= xAxisTitle,
-        yaxis_title= yAxisTitle,
-        font=dict(
-            family="Times New Roman",
-            size=9,
-            color="black"
+    ##--------- 1D Plotting --------------
+    # Check that the hists are 1 dimensional
+    if "TH1" in str(type(data_hist)) and "TH1" not in str(type(ref_hist)):
+        #Get bin centers from edges() stored by uproot
+        bins = data_hist.axes[0].edges();
+        if bins[0] < -999:
+            bins[0]=2*bins[1]-bins[2]
+    
+        #Get Titles for histogram, X-axis, Y-axis (Note data_hist.axes will have length > 1 if y-axis title is declared even with 1d plot)
+        xAxisTitle = data_hist.axes[0]._bases[0]._members["fTitle"]
+        if(len(data_hist.axes) > 1):
+            yAxisTitle = data_hist.axes[1]._bases[0]._members["fTitle"]
+        else:
+            yAxisTitle = ""
+        plotTitle = histpair.data_name + " KS Test  |  data:" + str(histpair.data_run) + " & ref:" + str(histpair.ref_run)
+    
+        #Plotly doesn't support #circ, #theta, #phi but it does support unicode
+        xAxisTitle = xAxisTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
+        yAxisTitle = yAxisTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
+        plotTitle = plotTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
+    
+    
+        #Plot histogram with previously declared axes and settings to look similar to PyRoot
+        c = go.Figure()
+        c.add_trace(go.Bar(name="data:"+str(histpair.data_run), x=bins, y=data_hist_norm, marker_color='white', marker=dict(line=dict(width=1,color='red'))))
+        c.add_trace(go.Bar(name="ref:"+str(histpair.ref_run), x=bins, y=ref_hist_norm, marker_color='rgb(204, 188, 172)', opacity=.9))
+        c['layout'].update(bargap=0)
+        c['layout'].update(barmode='overlay')
+        c['layout'].update(plot_bgcolor='white')
+        c.update_xaxes(showline=True, linewidth=2, linecolor='black', mirror=True, showgrid=False)
+        c.update_yaxes(showline=True, linewidth=2, linecolor='black', mirror=True, showgrid=False)
+        c.update_layout(
+            title=plotTitle , title_x=0.5,
+            xaxis_title= xAxisTitle,
+            yaxis_title= yAxisTitle,
+            font=dict(
+                family="Times New Roman",
+                size=9,
+                color="black"
+            )
         )
-    )
+        ref_text = "ref:"+str(histpair.ref_run)
+        data_text = "data:"+str(histpair.data_run)
+        #artifacts = [data_hist_norm, ref_hist_norm, data_text, ref_text]
+    ## --------- end 1D plotting ---------
+
+
+    ##---------- 2d Plotting --------------
+    # Check that the hists are 2 dimensional
+    if ( (       "TH2" in str(type(data_hist)) and       "TH2" in str(type(ref_hist)) ) or
+         ("TProfile2D" in str(type(data_hist)) and "TProfile2" in str(type(ref_hist)) ) ):
+        
+        colors = ['rgb(26, 42, 198)', 'rgb(118, 167, 231)', 'rgb(215, 226, 194)', 'rgb(212, 190, 109)', 'rgb(188, 76, 38)']
+        #Getting Plot labels for x-axis and y-axis as well as type (linear or categorical)
+        xLabels = None
+        yLabels = None
+        c = None
+        x_axis_type = 'linear'
+        y_axis_type = 'linear';
+        if data_hist.axes[0].labels():
+           xLabels = [str(x) for x in data_hist.axes[0].labels()]
+           x_axis_type = 'category'
+        else:
+           xLabels = [str(data_hist.axes[0]._members["fXmin"] + x * (data_hist.axes[0]._members["fXmax"]-data_hist.axes[0]._members["fXmin"])/data_hist.axes[0]._members["fNbins"]) for x in range(0,data_hist.axes[0]._members["fNbins"]+1)]
+    
+        if data_hist.axes[1].labels():
+           yLabels = [str(x) for x in data_hist.axes[1].labels()]
+           y_axis_type = 'category'
+        else:
+           yLabels = [str(data_hist.axes[1]._members["fXmin"] + x * (data_hist.axes[1]._members["fXmax"]-data_hist.axes[1]._members["fXmin"])/data_hist.axes[1]._members["fNbins"]) for x in range(0,data_hist.axes[1]._members["fNbins"]+1)]
+    
+        if("xlabels" in histpair.config.keys()):
+            xLabels=histpair.config["xlabels"]
+            x_axis_type = 'category'
+        if("ylabels" in histpair.config.keys()):
+            yLabels=histpair.config["ylabels"]
+            y_axis_type = 'category'
+    
+        pull_hist = numpy.transpose(pull_hist)
+    
+        #Getting Plot Titles for histogram, x-axis and y-axis
+        xAxisTitle = data_hist.axes[0]._bases[0]._members["fTitle"]
+        yAxisTitle = data_hist.axes[1]._bases[0]._members["fTitle"]
+        plotTitle = histpair.data_name + " Pull Values  |  data:" + str(histpair.data_run) + " & ref:" + str(histpair.ref_run)
+    
+        #Plotly doesn't support #circ, #theta, #phi but does support unicode
+        xAxisTitle = xAxisTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
+        yAxisTitle = yAxisTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
+        plotTitle = plotTitle.replace("#circ", "\u00B0").replace("#theta","\u03B8").replace("#phi","\u03C6").replace("#eta","\u03B7")
+    
+        #Plot pull-values using 2d heatmap will settings to look similar to old Pyroot version
+        c  = go.Figure(data=go.Heatmap(z=pull_hist, zmin=-pull_cap, zmax=pull_cap, colorscale=colors, x=xLabels, y=yLabels))
+        c['layout'].update(plot_bgcolor='white')
+        c.update_xaxes(showline=True, linewidth=2, linecolor='black', mirror=True, showgrid=False, type=x_axis_type)
+        c.update_yaxes(showline=True, linewidth=2, linecolor='black', mirror=True, showgrid=False, type=y_axis_type)
+        c.update_layout(
+            title=plotTitle , title_x=0.5,
+            xaxis_title= xAxisTitle,
+            yaxis_title= yAxisTitle,
+            font=dict(
+                family="Times New Roman",
+                size=9,
+                color="black"
+            )
+        )
+    ##----- end 2D plotting --------
+
+
 
 
     info = {
@@ -241,17 +313,16 @@ def ProbRel(Data, Ref, func, kurt=0):
     maxProb_down = Prob(exp_down, nData, Ref, nRef,func, kurt)
     maxProb = numpy.maximum(maxProb_up, maxProb_down)
     thisProb = Prob(Data, nData, Ref, nRef, func, kurt)
-    ## Sanity check to not have relative likelihood > 1
-    cond = maxProb < thisProb
 
-    if any(cond.flatten()):
-        print(f'for ProbRel')
-        print(f'Data: {Data[cond]}\nnData: {nData}\nRef: {Ref[cond]}\nnRef: {nRef}')
-
+    ## Sanity check to not have relative likelihood > 1    
     ## make sure check for thisProb < maxProb*0.001 (account for floating point inaccuracies) and just set the ratio to 1 if that is the case
     ratio = thisProb/maxProb
     cond = thisProb > maxProb*0.001
     ratio[cond] = 1
+
+     #if any(cond.flatten()):
+     #   print(f'for ProbRel')
+     #   print(f'Data: {Data[cond]}\nnData: {nData}\nRef: {Ref[cond]}\nnRef: {nRef}')
 
     return ratio #thisProb / maxProb
 
