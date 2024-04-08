ec37d3c20a5e29f005397a6b9af8fc12a4b72959
Caio Daumann <146199123+CaioDaumann@users.noreply.github.com>
2024-04-08 21:10:00 +0200

    Update beta_binomial.py to 0.5*threshold

--- plugins/beta_binomial.py
+++ plugins/beta_binomial.py
@@ -128,8 +128,9 @@ def beta_binomial(histpair, pull_cap=15, chi2_cut=10, pull_cut=10, min_entries=1
             if not sels[0] == 'BB': continue
             if sels[1] == 'Chi2':    sel_display_chi2    = float(sels[2])
             if sels[1] == 'MaxPull': sel_display_maxPull = float(sels[2])
+    
     # Defining the Selective display thresholds based on the Chi2 and MaxPull choosen thresholds 
-    if chi2 < chi2_cut*0.75 and max_pull < pull_cut*0.55 and not is_outlier:
+    if chi2 < chi2_cut*0.5 and max_pull < pull_cut*0.5 and not is_outlier:
         return None
 
     ## plotting
